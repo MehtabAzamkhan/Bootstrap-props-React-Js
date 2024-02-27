78fedfc75870f24cbcdbcc51ca05cd1c6a7bd6fe
MehtabAzamkhan <kmehtab285@gmail.comm>
2024-02-27 10:48:43 +0500

    cleaned code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-
-import './App.css';
-import Navbar from './components/Navbar';
-import Form from './components/Form';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Form from "./components/Form";
 function App() {
   return (
     <>
- <Navbar title="Azam" about="Heyy you"/>
-  <div className="container my-3"> 
-  <Form  heading="Enter text here"/>
-  </div>
+      <Navbar title="Azam" about="Heyy you" />
+      <div className="container my-3">
+        <Form heading="Enter text here" />
+      </div>
     </>
   );
 }
